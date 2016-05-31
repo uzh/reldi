a866d6912b6a137c13ca8d290c49ed0a72869f4a
Nikola Ljubešić <nljubesi@gmail.com>
2016-05-31 12:39:50 +0000

    README and tagging script updated.

--- tools/lib/README.md
+++ tools/lib/README.md
@@ -3,3 +3,6 @@
 If you do not have access rights, register first at http://nl.ijs.si/services/.
 
 There is also a nice web interface for testing the available technologies.
+
+The overall most informative file is the ```main.py``` script. If you need to tag and lemmatise a file or a directory of files (that have to end in '.txt'), the ```tag_all.py``` script can come in very handy.
+ 
--- tools/lib/tag_all.py
+++ tools/lib/tag_all.py
@@ -20,7 +20,7 @@ def write(result,file):
 if __name__ == "__main__":
     parser=argparse.ArgumentParser(description='Tagger and lemmatiser for Slovene, Croatian and Serbian\nUses the ReLDI API. For access rights visit http://nl.ijs.si/services/.')
     parser.add_argument('lang',help='language of the text(s)',choices=['sl','hr','sr'])
-    parser.add_argument('path',help='Path to the file or folder containing files to be tagged and lemmatised.')
+    parser.add_argument('path',help='Path to the file or folder containing files (with .txt extension) to be tagged and lemmatised.')
     args=parser.parse_args()
     passwd=getpass('Input password or user "'+user+'": ')
     tagger = Tagger(args.lang)
@@ -29,4 +29,5 @@ if __name__ == "__main__":
         write(eval(tagger.tagLemmatise(open(args.path).read().decode(coding).encode('utf8'))),open(args.path+'.taglem','w'))
     else:
         for file in os.listdir(args.path):
-            write(eval(tagger.tagLemmatise(open(os.path.join(args.path,file)).read().decode(coding).encode('utf8'))),open(os.path.join(args.path,file)+'.taglem','w'))
+            if file.endswith('.txt'):
+                write(eval(tagger.tagLemmatise(open(os.path.join(args.path,file)).read().decode(coding).encode('utf8'))),open(os.path.join(args.path,file)+'.taglem','w'))
