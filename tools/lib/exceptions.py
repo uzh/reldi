4188281b5517c7fe9c299ef6a7361dbc338db28d
Nikola Ljubešić <nljubesi@gmail.com>
2016-10-22 15:35:18 +0000

    Bunch of deletes

--- tools/lib/exceptions.py
+++
@@ -1,4 +0,0 @@
-class ApiException(Exception):
-    def __init__(self, message):
-        # Call the base class constructor with the parameters it needs
-        super(Exception, self).__init__(message)
